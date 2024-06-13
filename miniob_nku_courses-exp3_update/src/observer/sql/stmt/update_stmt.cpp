8c8115bf48e20dec6872a5b48ea879de81840a3b
yaohaowei0914 <973396597@qq.com>
2024-06-13 18:58:43 +0800

    add exp3

---
+++ miniob_nku_courses-exp3_update/src/observer/sql/stmt/update_stmt.cpp
@@ -0,0 +1,64 @@
+/* Copyright (c) 2021 OceanBase and/or its affiliates. All rights reserved.
+miniob is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         http://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details. */
+
+//
+// Created by Wangyunlai on 2022/5/22.
+//
+
+#include "sql/stmt/update_stmt.h"
+#include "common/log/log.h"
+#include "storage/db/db.h"
+#include "storage/table/table.h"
+
+UpdateStmt::UpdateStmt(Table *table, Value *values, FieldMeta* field_meta, FilterStmt* filter_stmt)
+    : table_(table), values_(values), field_meta_(field_meta), filter_stmt_(filter_stmt)
+{}
+
+RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
+{
+  const char *table_name = update.relation_name.c_str();
+  if (nullptr == db || nullptr == table_name) {
+    LOG_WARN("invalid argument. db=%p, table_name=%p",
+        db, table_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // check whether the table exists
+  Table *table = db->find_table(table_name);
+  if (nullptr == table) {
+    LOG_WARN("no such table. db=%s, table_name=%s", db->name(), table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  const TableMeta &table_meta = table->table_meta();
+  const FieldMeta* field_meta = table_meta.field(update.attribute_name.c_str());
+  if (nullptr == field_meta) {
+    LOG_WARN("no such field. field=%s.%s.%s", db->name(), table->name(), update.attribute_name.c_str());
+    return RC::SCHEMA_FIELD_MISSING;
+  }
+
+  // filter
+  std::unordered_map<std::string, Table *> table_map;
+  table_map.insert(std::pair<std::string, Table *>(std::string(table_name), table));
+
+  FilterStmt *filter_stmt = nullptr;
+  RC rc = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
+    return rc;
+  }
+
+  Value* values = new Value(update.value);
+
+  // TODO
+  stmt = new UpdateStmt(table, values, new FieldMeta(*field_meta), filter_stmt);
+  return RC::SUCCESS;
+}
